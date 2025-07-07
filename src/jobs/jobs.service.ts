5c1ba2e18eeb2bf7344545f1d17bf45aa2f6057f
AgasiD <38440249+AgasiD@users.noreply.github.com>
2025-07-07 12:14:35 -0300

    deploy

--- src/jobs/jobs.service.ts
+++ src/jobs/jobs.service.ts
@@ -19,7 +19,6 @@ export class JobsService {
     async initJobs() {
         const config = await this.configService.load_config();
         const cron = new Cron();
-        await obtenerBackup(this.client)
         cron.nuevo_job({
             tiempo: config.schedule_backup,
             callback: async () => await obtenerBackup(this.client)
